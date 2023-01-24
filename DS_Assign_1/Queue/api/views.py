6d8d40cc697fa2d7198521f9a71d21c6e97fe2e0
Esha Dinne <eshamanideep@iitkgp.ac.in>
2023-01-24 11:43:37 +0530

    minor update

--- DS_Assign_1/Queue/api/views.py
+++
@@ -1,3 +0,0 @@
-from rest_framework import generics
-
-# class GetTopicList()
--- DS_Assign_1/Queue/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class QueueConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Queue'
